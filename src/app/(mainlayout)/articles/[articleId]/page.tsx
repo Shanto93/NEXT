d4d422b0f5df1f0d8d5ad8f0e2c0d9ecd10e32ad
Shanto93 <shantoislam7363@gmail.com>
2025-04-21 15:57:09 +0600

    Navigating programmatically

--- src/app/(mainlayout)/articles/[articleId]/page.tsx
+++ src/app/(mainlayout)/articles/[articleId]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -9,12 +11,18 @@ interface Props {
   };
 }
 const ArticlePage = ({ params, searchParams }: Props) => {
-  const { articleId } = params;
-  const { lan = "en" } = searchParams;
+  const { articleId } = React.use(params);
+  const { lan = "en" } = React.use(searchParams);
+  const router = useRouter();
+
+  const handleGoHome = () => {
+    router.push("/");
+  };
   return (
     <div>
       <h1>Article in page {articleId} </h1>
       <p>Reading in language : {lan}</p>
+      <button onClick={handleGoHome}>Go Home</button>
     </div>
   );
 };
