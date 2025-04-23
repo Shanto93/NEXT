e7f3a9a1f92c20ef355eaaad0acb3cc1ad2eb43f
Shanto93 <shantoislam7363@gmail.com>
2025-04-23 10:01:40 +0600

    Recovering from error

--- src/app/(mainlayout)/reviews/error.tsx
+++ src/app/(mainlayout)/reviews/error.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-const error = ({ error }: { error: Error }) => {
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+const ErrorReview = ({ error, reset }: { error: Error; reset: () => void }) => {
+  const router = useRouter();
+
+  startTransition(() => {
+    router.refresh();
+    reset();
+  });
+
   return (
     <div>
       <h1>{error.message}</h1>
+      <button onClick={() => reset()}>Reload</button>
     </div>
   );
 };
 
-export default error;
+export default ErrorReview;
